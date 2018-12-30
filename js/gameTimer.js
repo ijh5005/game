e75047641a3e28623d6637997467b8e8595f8e82
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2018-12-29 20:09:54 -0500

    increment change

--- js/gameTimer.js
+++ js/gameTimer.js
@@ -15,9 +15,20 @@ const gameTimer = {
   stopTimer: () => {
     clearInterval(counter);
   },
+  getIncrement: (count) => {
+    let increment;
+    if (count < 1000) {
+      increment = 300;
+    } else if (count < 2000) {
+      increment = 150;
+    } else {
+      increment = 75;
+    }
+    return increment;
+  },
   incrementTimer: () => {
-    count += 300;
     let time;
+    count += gameTimer.getIncrement(count);
     const timeInString = count.toString();
     const length = timeInString.length;
     if (length < 4) {
